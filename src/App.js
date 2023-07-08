332ec6d03c382cdb89c2d86db2fb4811b0368954
Diem <diemnguyen.16032002dmx@gmail.com>
2023-07-08 12:58:45 +0700

    'u'

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import './scss/style.scss'
 import ForgotPassword from './pages/forgotpassword/ForgotPassword';
+import Header from './components/layouts/Header';
+import Layout from './components/layouts/Main/Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Layout />}>
+          
+        </Route> 
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword/>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/layouts/Main/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './../Header/index';
+
+import '../../../styles/header.css'
+
+const Layout = () => {
+  return (
+    <>
+        <Header/>     
+
+    </>
+  )
+}
+
+export default Layout
