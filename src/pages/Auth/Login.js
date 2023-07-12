edae7b9c52ec84cf55fa0886e70a6e38aa6e219d
Chung0902 <chungnguyen.130902@gmail.com>
2023-07-12 15:10:24 +0700

    up

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 import {
   CButton,
   CCard,
@@ -12,10 +12,10 @@ import {
   CInputGroup,
   CInputGroupText,
   CRow,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilLockLocked, cilUser } from '@coreui/icons'
-import "../../styles/buttonlogin.css"
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import { cilLockLocked, cilUser } from "@coreui/icons";
+import "../../styles/buttonlogin.css";
 
 const Login = () => {
   return (
@@ -28,7 +28,9 @@ const Login = () => {
                 <CCardBody>
                   <CForm>
                     <h1>Login</h1>
-                    <p className="text-medium-emphasis">Sign In to your account</p>
+                    <p className="text-medium-emphasis">
+                      Sign In to your account
+                    </p>
                     <CInputGroup className="mb-3">
                       <CInputGroupText>
                         <CIcon icon={cilUser} />
@@ -47,30 +49,39 @@ const Login = () => {
                     </CInputGroup>
                     <CRow>
                       <CCol xs={6}>
-                        <CButton color="primary" className="px-4 buttonlg">
-                          Login
-                        </CButton>
+                        <CButton className="px-4 buttonlg">Login</CButton>
                       </CCol>
                       <CCol xs={6} className="text-right">
-                      <Link to="/forgot-password">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </Link>
+                        <Link to="/forgot-password">
+                          <CButton color="link" className="px-0">
+                            Forgot password?
+                          </CButton>
+                        </Link>
                       </CCol>
                     </CRow>
                   </CForm>
                 </CCardBody>
               </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+              <CCard
+                className="text-white bg-primary py-5"
+                style={{ width: "44%" }}
+              >
                 <CCardBody className="text-center">
                   <div>
                     <h2>Sign up</h2>
                     <p>
-                    When logging into the system of a website or application, the user will need to provide login information to authenticate his/her identity and access the system's features and services.
+                      When logging into the system of a website or application,
+                      the user will need to provide login information to
+                      authenticate his/her identity and access the system's
+                      features and services.
                     </p>
                     <Link to="/register">
-                      <CButton color="primary" className="mt-3 " active tabIndex={-1}>
+                      <CButton
+                        color="primary"
+                        className="mt-3 "
+                        active
+                        tabIndex={-1}
+                      >
                         Register Now!
                       </CButton>
                     </Link>
@@ -82,7 +93,7 @@ const Login = () => {
         </CRow>
       </CContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
