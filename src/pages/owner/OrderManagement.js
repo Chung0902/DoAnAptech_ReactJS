43bc79d945bfe05fa5b4922fc53de36f88a0ad09
Chung0902 <chungnguyen.130902@gmail.com>
2023-07-29 14:16:33 +0700

    up

--- src/pages/owner/OrderManagement.js
+++ src/pages/owner/OrderManagement.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import axiosClient from "../../libraries/axiosClient";
 import BrowsOrder from "./orders/BrowsOrder";
 
 const OrderManagement = () => {
+  const [listorders, setListorders] = useState([]);
+
+  //hiển thị danh sách đơn hàng
+  const getlistorders = async () => {
+    try {
+      const response = await axiosClient.get("questions/listorders");
+      setListorders(response.payload);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getlistorders();
+  }, []);
+
   return (
     <main className="app-content">
       <div className="app-title">
@@ -20,21 +37,6 @@ const OrderManagement = () => {
           <div className="tile">
             <div className="tile-body">
               <div className="row element-button">
-                <div className="col-sm-2">
-                  <NavLink
-                    to="/main/ordermanagement/addorders"
-                    className="active1"
-                  >
-                    <a
-                      className="btn btn-add btn-sm"
-                      href="form-add-don-hang.html"
-                      title="Thêm"
-                    >
-                      <i className="fas fa-plus"></i>
-                      Tạo mới đơn hàng
-                    </a>
-                  </NavLink>
-                </div>
                 <div className="col-sm-2">
                   <a
                     className="btn btn-delete btn-sm"
@@ -64,209 +66,47 @@ const OrderManagement = () => {
                     <th>Tính năng</th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr>
-                    <td width="10">
-                      <input type="checkbox" name="check1" value="1" />
-                    </td>
-                    <td>MD0837</td>
-                    <td>Triệu Thanh Phú</td>
-                    <td>Ghế làm việc Zuno, Bàn ăn gỗ Theresa</td>
-                    <td>2</td>
-                    <td>9.400.000 đ</td>
-                    <td>
-                      <span className="badge bg-success">Hoàn thành</span>
-                    </td>
-                    <td>
-                      <button
-                        className="btn btn-primary btn-sm trash"
-                        type="button"
-                        title="Xóa"
-                      >
-                        <i className="fas fa-trash-alt"></i>{" "}
-                      </button>
-                      <button
-                        className="btn btn-primary btn-sm edit"
-                        type="button"
-                        title="Duyệt"
-                        id="show-emp"
-                        data-bs-toggle="modal"
-                        data-bs-target="#ModalUP"
-                      >
-                        <i className="fas fa-edit"></i>
-                        <BrowsOrder/>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td width="10">
-                      <input type="checkbox" name="check1" value="1" />
-                    </td>
-                    <td>MĐ8265</td>
-                    <td>Nguyễn Thị Ngọc Cẩm</td>
-                    <td>Ghế ăn gỗ Lucy màu trắng</td>
-                    <td>1</td>
-                    <td>3.800.000 đ</td>
-                    <td>
-                      <span className="badge bg-success">Hoàn thành</span>
-                    </td>
-                    <td>
-                      <button
-                        className="btn btn-primary btn-sm trash"
-                        type="button"
-                        title="Xóa"
-                      >
-                        <i className="fas fa-trash-alt"></i>{" "}
-                      </button>
-                      <button
-                        className="btn btn-primary btn-sm edit"
-                        type="button"
-                        title="Duyệt"
-                        id="show-emp"
-                        data-bs-toggle="modal"
-                        data-bs-target="#ModalUP"
-                      >
-                        <i className="fas fa-edit"></i>
-                        <BrowsOrder/>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td width="10">
-                      <input type="checkbox" name="check1" value="1" />
-                    </td>
-                    <td>MT9835</td>
-                    <td>Đặng Hoàng Phúc</td>
-                    <td>
-                      Giường ngủ Jimmy, Bàn ăn mở rộng cao cấp Dolas, Ghế làm
-                      việc Zuno
-                    </td>
-                    <td>3 </td>
-                    <td>40.650.000 đ</td>
-                    <td>
-                      <span className="badge bg-success">Hoàn thành</span>
-                    </td>
-                    <td>
-                      <button
-                        className="btn btn-primary btn-sm trash"
-                        type="button"
-                        title="Xóa"
-                      >
-                        <i className="fas fa-trash-alt"></i>{" "}
-                      </button>
-                      <button
-                        className="btn btn-primary btn-sm edit"
-                        type="button"
-                        title="Duyệt"
-                        id="show-emp"
-                        data-bs-toggle="modal"
-                        data-bs-target="#ModalUP"
-                      >
-                        <i className="fas fa-edit"></i>
-                        <BrowsOrder/>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td width="10">
-                      <input type="checkbox" name="check1" value="1" />
-                    </td>
-                    <td>ER3835</td>
-                    <td>Nguyễn Thị Mỹ Yến</td>
-                    <td>Bàn ăn mở rộng Gepa</td>
-                    <td>1 </td>
-                    <td>16.770.000 đ</td>
-                    <td>
-                      <span className="badge bg-info">Chờ thanh toán</span>
-                    </td>
-                    <td>
-                      <button
-                        className="btn btn-primary btn-sm trash"
-                        type="button"
-                        title="Xóa"
-                      >
-                        <i className="fas fa-trash-alt"></i>{" "}
-                      </button>
-                      <button
-                        className="btn btn-primary btn-sm edit"
-                        type="button"
-                        title="Duyệt"
-                        id="show-emp"
-                        data-bs-toggle="modal"
-                        data-bs-target="#ModalUP"
-                      >
-                        <i className="fas fa-edit"></i>
-                        <BrowsOrder/>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td width="10">
-                      <input type="checkbox" name="check1" value="1" />
-                    </td>
-                    <td>AL3947</td>
-                    <td>Phạm Thị Ngọc</td>
-                    <td>Bàn ăn Vitali mặt đá, Ghế ăn gỗ Lucy màu trắng</td>
-                    <td>2 </td>
-                    <td>19.770.000 đ</td>
-                    <td>
-                      <span className="badge bg-warning">Đang giao hàng</span>
-                    </td>
-                    <td>
-                      <button
-                        className="btn btn-primary btn-sm trash"
-                        type="button"
-                        title="Xóa"
-                      >
-                        <i className="fas fa-trash-alt"></i>{" "}
-                      </button>
-                      <button
-                        className="btn btn-primary btn-sm edit"
-                        type="button"
-                        title="Duyệt"
-                        id="show-emp"
-                        data-bs-toggle="modal"
-                        data-bs-target="#ModalUP"
-                      >
-                        <i className="fas fa-edit"></i>
-                        <BrowsOrder/>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td width="10">
-                      <input type="checkbox" name="check1" value="1" />
-                    </td>
-                    <td>QY8723</td>
-                    <td>Ngô Thái An</td>
-                    <td>Giường ngủ Kara 1.6x2m</td>
-                    <td>1 </td>
-                    <td>14.500.000 đ</td>
-                    <td>
-                      <span className="badge bg-danger">Đã hủy</span>
-                    </td>
-                    <td>
-                      <button
-                        className="btn btn-primary btn-sm trash"
-                        type="button"
-                        title="Xóa"
-                      >
-                        <i className="fas fa-trash-alt"></i>{" "}
-                      </button>
-                      <button
-                        className="btn btn-primary btn-sm edit"
-                        type="button"
-                        title="Duyệt"
-                        id="show-emp"
-                        data-bs-toggle="modal"
-                        data-bs-target="#ModalUP"
-                      >
-                        <i className="fas fa-edit"></i>
-                        <BrowsOrder/>
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
+                {listorders &&
+                  listorders.map((e) => (
+                    <tbody key={e.order._id}>
+                      {e.orderDetails.map((orderDetail) => (
+                        <tr key={orderDetail.productId}>
+                          <td width="10">
+                            <input type="checkbox" name="check1" value="1" />
+                          </td>
+                          <td>{orderDetail.productId}</td>
+                          <td>
+                            {e.order.customer.firstName}
+                            {e.order.customer.lastName}
+                          </td>
+                          <td>{orderDetail.productName}</td>
+                          <td>{orderDetail.quantity}</td>
+                          <td>{orderDetail.totalOrderDetailPrice} đ</td>
+                          <td>{e.order.status}</td>
+                          <td>
+                            <button
+                              className="btn btn-primary btn-sm trash"
+                              type="button"
+                              title="Xóa"
+                            >
+                              <i className="fas fa-trash-alt"></i>{" "}
+                            </button>
+                            <button
+                              className="btn btn-primary btn-sm edit"
+                              type="button"
+                              title="Duyệt"
+                              id="show-emp"
+                              data-bs-toggle="modal"
+                              data-bs-target="#ModalUP"
+                            >
+                              <i className="fas fa-edit"></i>
+                              <BrowsOrder />
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  ))}
               </table>
             </div>
           </div>
