59ad974f1ed87649a9f62a60bdb944da0b8130e9
Chung0902 <chungnguyen.130902@gmail.com>
2023-07-31 22:47:00 +0700

    up

--- src/pages/owner/EmployeeManager.js
+++ src/pages/owner/EmployeeManager.js
@@ -1,27 +1,26 @@
-import React, {useState} from 'react'
-import "../../../src/styles/main.css"
-import { NavLink } from 'react-router-dom';
-import UpdateEmployee from './employees/UpdateEmployee';
-import axiosClient from '../../libraries/axiosClient';
-import { useEffect } from 'react';
-import { toast } from 'react-hot-toast';
-import { useAuth } from '../../context/auth';
-
+import React, { useState } from "react";
+import "../../../src/styles/main.css";
+import { NavLink } from "react-router-dom";
+import UpdateEmployee from "./employees/UpdateEmployee";
+import axiosClient from "../../libraries/axiosClient";
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+import { useAuth } from "../../context/auth";
 
 const EmployeeManager = () => {
   const [employees, setEmployees] = useState([]);
-  const [firstName,setFirstName] = useState();
-  const [birthday,setBirthday]= useState();
-  const [address,setAddress] = useState();
-  const [lastName,setLastName] = useState();
-  const [phoneNumber,setPhoneNumber]= useState();
-  const [email,setEmail] = useState();
-  const [sex,setSex]= useState();
-  const [role,setRole] = useState();
-  const [question,setQuestion] = useState();
-  const [password,setPassword] = useState ();
-  const [visible,setVisible]= useState(false)
-  const [selected,setSelected] = useState(null)
+  const [firstName, setFirstName] = useState();
+  const [birthday, setBirthday] = useState();
+  const [address, setAddress] = useState();
+  const [lastName, setLastName] = useState();
+  const [phoneNumber, setPhoneNumber] = useState();
+  const [email, setEmail] = useState();
+  const [sex, setSex] = useState();
+  const [role, setRole] = useState();
+  const [question, setQuestion] = useState();
+  const [password, setPassword] = useState();
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(null);
   const [updateFirstName, setUpdateFirstName] = useState();
   const [updateLastName, setUpdateLastName] = useState();
   const [updatePhoneNumber, setUpdatePhoneNumber] = useState();
@@ -33,11 +32,11 @@ const EmployeeManager = () => {
     const selectedRole = event.target.value;
     let roleCode;
 
-    if (selectedRole === 'Bán hàng') {
+    if (selectedRole === "Bán hàng") {
       roleCode = 0;
-    } else if (selectedRole === 'Shipper') {
+    } else if (selectedRole === "Shipper") {
       roleCode = 2;
-    } else if (selectedRole === 'Chủ shop') {
+    } else if (selectedRole === "Chủ shop") {
       roleCode = 1;
     }
 
@@ -46,82 +45,102 @@ const EmployeeManager = () => {
 
   const getAllEmployees = async () => {
     try {
-      const response = await axiosClient.get('admin/employees');
+      const response = await axiosClient.get("admin/employees");
       setEmployees(response.payload);
-      
     } catch (error) {
       console.error(error);
     }
   };
 
-    useEffect(() =>{
-      getAllEmployees();
-    },[]);
-    
+  useEffect(() => {
+    getAllEmployees();
+  }, []);
 
-    const handleUpdate = async (e) =>{
-      e.preventDefault();
-      try {
-        const authToken = `Bearer ${auth.token}`; // Replace with your actual authentication token
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const authToken = `Bearer ${auth.token}`; // Replace with your actual authentication token
 
-        const config = {
-          headers: {
-            'Authorization': authToken,
-          }
-        };
+      const config = {
+        headers: {
+          Authorization: authToken,
+        },
+      };
 
-          const response = await axiosClient.patch(`admin/employees/${selected._id}`, {firstName: updateFirstName,lastName: updateLastName,phoneNumber: updatePhoneNumber,email: updateEmail,address: updateAddress,role : role},config);
-          if(response.success){
-              toast.success(" is updated");
-              setSelected(null);
-              setUpdateFirstName("");
-              setUpdateLastName("");
-              setUpdateEmail("");
-              setUpdateAddress("");
-              setUpdatePhoneNumber("");
-              setRole("")
-              setEmployees(employees.map((employee) => {
-                if (employee._id === selected._id) {
-                  return { ...employee,firstName: updateFirstName,lastName: updateLastName,phoneNumber: updatePhoneNumber,email: updateEmail,address: updateAddress,role : role}; // Cập nhật tên của danh mục tương ứng
-                }
-                return employee;
-              }));
-          }
-    
-      } catch (error) {
-        console.log(error.message);
-          toast.error('Something went wrong')
+      const response = await axiosClient.patch(
+        `admin/employees/${selected._id}`,
+        {
+          firstName: updateFirstName,
+          lastName: updateLastName,
+          phoneNumber: updatePhoneNumber,
+          email: updateEmail,
+          address: updateAddress,
+          role: role,
+        },
+        config
+      );
+      if (response.success) {
+        toast.success(" is updated");
+        setSelected(null);
+        setUpdateFirstName("");
+        setUpdateLastName("");
+        setUpdateEmail("");
+        setUpdateAddress("");
+        setUpdatePhoneNumber("");
+        setRole("");
+        setEmployees(
+          employees.map((employee) => {
+            if (employee._id === selected._id) {
+              return {
+                ...employee,
+                firstName: updateFirstName,
+                lastName: updateLastName,
+                phoneNumber: updatePhoneNumber,
+                email: updateEmail,
+                address: updateAddress,
+                role: role,
+              }; // Cập nhật tên của danh mục tương ứng
+            }
+            return employee;
+          })
+        );
       }
-    };
-    //Delete category
-    const handleDelete = async (pId) =>{
-      try {
-        const authToken = `Bearer ${auth.token}`; // Replace with your actual authentication token
-
-        const config = {
-          headers: {
-            'Authorization': authToken,
-          }
-        };
-
-          const response = await axiosClient.delete(`admin/employees/${pId}`,config);
-          if(response?.success){
-              toast.success(`category is deleted`);
-              setEmployees(employees.filter((employee) => employee._id !== pId)); // Loại bỏ danh mục đã được xóa khỏi danh sách
-          }
-          
-      } catch (error) {
-          toast.error('Something went wrong')
-      }
+    } catch (error) {
+      console.log(error.message);
+      toast.error("Something went wrong");
+    }
   };
-    // Hàm biến đổi định dạng ngày sinh
-    const formatDate = (dateString) => {
-      const date = new Date(dateString);
-      const day = date.getDate().toString().padStart(2, '0');
-      const month = (date.getMonth() + 1).toString().padStart(2, '0');
-      const year = date.getFullYear().toString().slice(0);
-      return `${day}/${month}/${year}`;
-    };
+  //Delete category
+  const handleDelete = async (pId) => {
+    try {
+      const authToken = `Bearer ${auth.token}`; // Replace with your actual authentication token
+
+      const config = {
+        headers: {
+          Authorization: authToken,
+        },
+      };
+
+      const response = await axiosClient.delete(
+        `admin/employees/${pId}`,
+        config
+      );
+      if (response?.success) {
+        toast.success(`category is deleted`);
+        setEmployees(employees.filter((employee) => employee._id !== pId)); // Loại bỏ danh mục đã được xóa khỏi danh sách
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+  // Hàm biến đổi định dạng ngày sinh
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear().toString().slice(0);
+    return `${day}/${month}/${year}`;
+  };
   return (
     <main className="app-content">
       <div className="app-title">
@@ -145,16 +164,12 @@ const EmployeeManager = () => {
                     to="/main/employeemanager/addemployee"
                     className="active1"
                   >
-                    
-                      <i className="fas fa-plus"></i>
-                      Tạo mới nhân viên
-                    
+                    <i className="fas fa-plus"></i>
+                    Tạo mới nhân viên
                   </NavLink>
                 </div>
                 <div className="col-sm-2">
-                  <a
-                    className="btn btn-delete btn-sm"
-                  >
+                  <a className="btn btn-delete btn-sm">
                     <i className="fas fa-trash-alt"></i> Xóa tất cả{" "}
                   </a>
                 </div>
@@ -183,72 +198,79 @@ const EmployeeManager = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {employees && employees.map((e) => e.role !== 1 ? (
-                  <tr key={e._id}>
-                    <td width="10">
-                      <input type="checkbox" name="check1" value="1" />
-                    </td>
-                    <td>{e._id}</td>
-                    <td>{e.firstName}{e.lastName}</td>
-                    <td>
-                      <img
-                        className="img-card-person"
-                        src="/img-anhthe/1.jpg"
-                        alt=""
-                      />
-                    </td>
-                    <td>{e.address}</td>
-                    <td>{formatDate(e.birthday)}</td>
-                    <td>{e.sex}</td>
-                    <td>{e.phoneNumber}</td>
-                    <td>{e.role === 0 ? 'Bán hàng' : 'Giao hàng'}</td>
-                    <td className="table-td-center">
-                      <button
-                        className="btn btn-primary btn-sm trash"
-                        type="button"
-                        title="Xóa"
-                        onClick={() => { handleDelete(e._id)}}
-                      >
-                        <i className="fas fa-trash-alt"></i>
-                      </button>               
-                        <button
-                          type="button"
-                          className="btn btn-primary"
-                          data-bs-toggle="modal"
-                          data-bs-target="#exampleModal"
-                          data-bs-whatever="@mdo"
-                          onClick={() => {
-                            setVisible(true) ;
-                            setSelected(e);
-                            setUpdateFirstName(e.firstName);
-                            setUpdateLastName(e.lastName);
-                            setUpdatePhoneNumber(e.phoneNumber);
-                            setUpdateEmail(e.email);
-                            setUpdateAddress(e.address);
-                            setRole(e.role)
-                            }}
-                        >
-                          <i className="fas fa-edit"></i>
-                        </button>
-                        <UpdateEmployee 
-                        handleSubmit={handleUpdate} 
-                        firstName={updateFirstName} 
-                        lastName={updateLastName} 
-                        phoneNumber={updatePhoneNumber} 
-                        email={updateEmail} 
-                        address={updateAddress} 
-                        setEmail={setUpdateEmail} 
-                        setLastName={setUpdateLastName} 
-                        setPhoneNumber={setUpdatePhoneNumber} 
-                        setFirstName={setUpdateFirstName}
-                        setAddress={setUpdateAddress} 
-                        handleRoleChange={handleRoleChange} />
-                
-                    </td>
-                  </tr>) : (
-                      ""
-                  ))}
-  
+                  {employees &&
+                    employees.map((e) =>
+                      e.role !== 1 ? (
+                        <tr key={e._id}>
+                          <td width="10">
+                            <input type="checkbox" name="check1" value="1" />
+                          </td>
+                          <td>{e._id}</td>
+                          <td>
+                            {e.firstName} {e.lastName}
+                          </td>
+                          <td>
+                            <img
+                              className="img-card-person"
+                              src="/img-anhthe/1.jpg"
+                              alt=""
+                            />
+                          </td>
+                          <td>{e.address}</td>
+                          <td>{formatDate(e.birthday)}</td>
+                          <td>{e.sex}</td>
+                          <td>{e.phoneNumber}</td>
+                          <td>{e.role === 0 ? "Bán hàng" : "Giao hàng"}</td>
+                          <td className="table-td-center">
+                            <button
+                              className="btn btn-primary btn-sm trash"
+                              type="button"
+                              title="Xóa"
+                              onClick={() => {
+                                handleDelete(e._id);
+                              }}
+                            >
+                              <i className="fas fa-trash-alt"></i>
+                            </button>
+                            <button
+                              type="button"
+                              className="btn btn-primary"
+                              data-bs-toggle="modal"
+                              data-bs-target="#exampleModal"
+                              data-bs-whatever="@mdo"
+                              onClick={() => {
+                                setVisible(true);
+                                setSelected(e);
+                                setUpdateFirstName(e.firstName);
+                                setUpdateLastName(e.lastName);
+                                setUpdatePhoneNumber(e.phoneNumber);
+                                setUpdateEmail(e.email);
+                                setUpdateAddress(e.address);
+                                setRole(e.role);
+                              }}
+                            >
+                              <i className="fas fa-edit"></i>
+                            </button>
+                            <UpdateEmployee
+                              handleSubmit={handleUpdate}
+                              firstName={updateFirstName}
+                              lastName={updateLastName}
+                              phoneNumber={updatePhoneNumber}
+                              email={updateEmail}
+                              address={updateAddress}
+                              setEmail={setUpdateEmail}
+                              setLastName={setUpdateLastName}
+                              setPhoneNumber={setUpdatePhoneNumber}
+                              setFirstName={setUpdateFirstName}
+                              setAddress={setUpdateAddress}
+                              handleRoleChange={handleRoleChange}
+                            />
+                          </td>
+                        </tr>
+                      ) : (
+                        ""
+                      )
+                    )}
                 </tbody>
               </table>
             </div>
@@ -257,6 +279,6 @@ const EmployeeManager = () => {
       </div>
     </main>
   );
-}
+};
 
-export default EmployeeManager
+export default EmployeeManager;
