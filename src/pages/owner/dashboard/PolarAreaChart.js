fc0243c416769d4be0c7d6f0a3a7a4f62b4b9e89
Chung0902 <chungnguyen.130902@gmail.com>
2023-08-02 16:35:37 +0700

    up

--- src/pages/owner/dashboard/PolarAreaChart.js
+++ src/pages/owner/dashboard/PolarAreaChart.js
@@ -5,26 +5,33 @@ function getStyle(property) {
   return getComputedStyle(document.documentElement).getPropertyValue(property);
 }
 
-const PolarAreaChart = () => {
+const PolarAreaChart = (props) => {
+  // Trường hợp không có dữ liệu, hiển thị thông báo hoặc một biểu đồ mặc định
+  if (!props.data || props.data.length === 0) {
+    return <div>Không có dữ liệu để hiển thị biểu đồ</div>;
+  }
+
+  const chartData = {
+    labels: props.data.map((product) => product.name),
+    datasets: [
+      {
+        data: props.data.map((product) => product.totalQuantity),
+        backgroundColor: [
+          "#FF6384",
+          "#4BC0C0",
+          "#FFCE56",
+          "#E7E9ED",
+          "#36A2EB",
+        ],
+      },
+    ],
+  };
+
   return (
     <>
       <CChart
         type="polarArea"
-        data={{
-          labels: ["Red", "Green", "Yellow", "Grey", "Blue"],
-          datasets: [
-            {
-              data: [11, 16, 7, 3, 14],
-              backgroundColor: [
-                "#FF6384",
-                "#4BC0C0",
-                "#FFCE56",
-                "#E7E9ED",
-                "#36A2EB",
-              ],
-            },
-          ],
-        }}
+        data={chartData}
         options={{
           plugins: {
             legend: {
