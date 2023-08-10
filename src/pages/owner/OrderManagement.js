610c7e1de1438728e24522a11246a567b0120e70
Diem <diemnguyen.16032002dmx@gmail.com>
2023-08-10 18:34:00 +0700

    'u'

--- src/pages/owner/OrderManagement.js
+++ src/pages/owner/OrderManagement.js
@@ -2,9 +2,45 @@ import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import axiosClient from "../../libraries/axiosClient";
 import BrowsOrder from "./orders/BrowsOrder";
+import { useAuth } from "../../context/auth";
 
 const OrderManagement = () => {
   const [listorders, setListorders] = useState([]);
+  const [orders,setOrders]=useState([])
+  const [auth, setAuth] = useAuth();
+
+  // Hàm gọi API để cập nhật trạng thái và mã nhân viên
+  const updateOrder = async (orderId, employeeId, paymentType,customerId) => {
+    try {
+      const response = await axiosClient.patch(`admin/orders/${orderId}`, {
+        status: "DELIVERING",
+        employeeId,
+        paymentType,
+        customerId
+        
+      });
+      // Cập nhật lại danh sách đơn hàng sau khi cập nhật thành công
+      setListorders((prevList) =>
+        prevList.map((order) => {
+          if (order.order._id === orderId) {
+            return {
+              ...order,
+              order: {
+                ...order.order,
+                status: "DELIVERING",
+                employeeId,
+                paymentType,
+                customerId
+              },
+            };
+          }
+          return order;
+        })
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   //hiển thị danh sách đơn hàng
   const getlistorders = async () => {
@@ -49,85 +85,84 @@ const OrderManagement = () => {
                 </div>
               </div>
               <div className="tile-body">
-              <table
-                className="table table-hover table-bordered"
-                id="sampleTable"
-              >
-                <thead>
-                  <tr>
-                    <th width="10">
-                      <input type="checkbox" id="all" />
-                    </th>
-                    <th>ID đơn hàng</th>
-                    <th>Khách hàng</th>
-                    <th>Sản phẩm</th>
-                    <th>Số lượng</th>
-                    <th>Tổng tiền</th>
-                    <th>Tổng cộng</th>
-                    <th>Tình trạng</th>
-                    <th>Tính năng</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {listorders &&
-                    listorders.map((e) => (
-                      <React.Fragment key={e.order._id}>
-                        {/* Hiển thị thông tin khách hàng cho mỗi đơn hàng */}
-                        <tr>
-                          <td rowSpan={e.orderDetails.length + 1}>
-                            <input type="checkbox" name="check1" value="1" />
-                          </td>
-                          <td rowSpan={e.orderDetails.length + 1}>
-                            {e.order._id}
-                          </td>
-                          <td rowSpan={e.orderDetails.length + 1}>
-                            {e.order.customer.firstName}{" "}
-                            {e.order.customer.lastName}
-                          </td>
-                        </tr>
-                        {/* Hiển thị thông tin sản phẩm cho mỗi đơn hàng */}
-                        {e.orderDetails.map((orderDetail) => (
-                          <tr key={orderDetail.productId}>
-                            <td>{orderDetail.productName}</td>
-                            <td>{orderDetail.quantity}</td>
-                            <td>{e.totalamountdiscount} đ</td>
-                            {e.orderDetails.indexOf(orderDetail) === 0 && (
-                              <td
-                                className="totalor"
-                                rowSpan={e.orderDetails.length + 1}
-                              >
-                                {e.totalOrderPrice} đ
-                              </td>
-                            )}
-                            {e.orderDetails.indexOf(orderDetail) === 0 && (
-                              <td
-                                className="status_or"
-                                rowSpan={e.orderDetails.length + 1}
-                              >
-                                {e.order.status}
-                              </td>
-                            )}
-                            {e.orderDetails.indexOf(orderDetail) === 0 && (
-                              <td rowSpan={e.orderDetails.length + 1}>
-                                <button
-                                  className="btn btn-primary btn-sm edit"
-                                  type="button"
-                                  title="Duyệt"
-                                  id="show-emp"
-                                  data-bs-toggle="modal"
-                                  data-bs-target="#ModalUP"
-                                >
-                                  <i className="fas fa-edit"></i>
-                                  <BrowsOrder />
-                                </button>
-                              </td>
-                            )}
+                <table
+                  className="table table-hover table-bordered"
+                  id="sampleTable"
+                >
+                  <thead>
+                    <tr>
+                      <th width="10">
+                        <input type="checkbox" id="all" />
+                      </th>
+                      <th>ID đơn hàng</th>
+                      <th>Khách hàng</th>
+                      <th>Sản phẩm</th>
+                      <th>Số lượng</th>
+                      <th>Tổng tiền</th>
+                      <th>Tổng cộng</th>
+                      <th>Tình trạng</th>
+                      <th>Tính năng</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {listorders &&
+                      listorders.map((e) => (
+                        <React.Fragment key={e.order._id}>
+                          {/* Hiển thị thông tin khách hàng cho mỗi đơn hàng */}
+                          <tr>
+                            <td rowSpan={e.orderDetails.length + 1}>
+                              <input type="checkbox" name="check1" value="1" />
+                            </td>
+                            <td rowSpan={e.orderDetails.length + 1}>
+                              {e.order._id}
+                            </td>
+                            <td rowSpan={e.orderDetails.length + 1}>
+                              {e.order.customer.firstName}{" "}
+                              {e.order.customer.lastName}
+                            </td>
                           </tr>
-                        ))}
-                      </React.Fragment>
-                    ))}
-                </tbody>
-              </table>
+                          {/* Hiển thị thông tin sản phẩm cho mỗi đơn hàng */}
+                          {e.orderDetails.map((orderDetail) => (
+                            <tr key={orderDetail.productId}>
+                              <td>{orderDetail.productName}</td>
+                              <td>{orderDetail.quantity}</td>
+                              <td>{e.totalamountdiscount} đ</td>
+                              {e.orderDetails.indexOf(orderDetail) === 0 && (
+                                <td
+                                  className="totalor"
+                                  rowSpan={e.orderDetails.length + 1}
+                                >
+                                  {e.totalOrderPrice} đ
+                                </td>
+                              )}
+                              {e.orderDetails.indexOf(orderDetail) === 0 && (
+                                <td
+                                  className="status_or"
+                                  rowSpan={e.orderDetails.length + 1}
+                                >
+                                  {e.order.status}
+                                </td>
+                              )}
+                              {e.orderDetails.indexOf(orderDetail) === 0 && (
+                                <td rowSpan={e.orderDetails.length + 1}>
+                                  <button
+                                    className="btn btn-primary btn-sm edit"
+                                    type="button"
+                                    title="Duyệt"
+                                    onClick={() =>
+                                      updateOrder(e.order._id, auth.user._id,e.order.paymentType,e.order.customer._id)
+                                    }
+                                  >
+                                    <i className="fas fa-edit"></i>
+                                  </button>
+                                </td>
+                              )}
+                            </tr>
+                          ))}
+                        </React.Fragment>
+                      ))}
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
